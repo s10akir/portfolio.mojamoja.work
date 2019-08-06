57213428ee9442a289a0ec3c4da85892651b64fd
Akira Shinohara <k017c1067@it-neec.jp>
2019-08-06 09:01:22 +0000

    [new] sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <v-app>
     <v-content>
-      <router-view></router-view>
+      <v-container fluid ma-0 pa-0>
+        <v-layout>
+          <v-flex md4 id="sidebar" pa-4 teal darken-2>
+            <sidebar />
+          </v-flex>
+          <v-flex md8>
+            <router-view></router-view>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import sidebar from "./components/SideBar"
 
 export default {
   name: 'App',
   components: {
+    sidebar
   },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style scoped lang="sass">
+#sidebar
+  height: 100vh
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sidebar">
+    <v-avatar size="30%">
+      <v-img
+        :src="require('../assets/avatar.jpg')"
+      />
+    </v-avatar>
+    <h1 class="display-1">Akira Shinohara</h1>
+    <p class="body-1">1998-10-02</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar'
+}
+</script>
+
+<style scoped lang="sass">
+.sidebar
+  text-align: center
+</style>
