d0ec3a792072e9c121fbba6b47b8e88c095542d2
Akira Shinohara <k017c1067@it-neec.jp>
2019-08-08 04:39:28 +0000

    [new] mobile nav

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,38 @@
   <v-app>
     <v-content>
       <v-container fluid ma-0 pa-0>
-        <v-layout>
-          <transition>
+        <v-layout wrap>
           <v-flex id="sidebar" md4 sm4 hidden-xs-only :class="color" px-4 py-12>
             <sidebar />
           </v-flex>
-          </transition>
+          <v-flex xs12 hidden-sm-and-up>
+            <v-toolbar class="px-0">
+              <v-container fluid>
+                <v-layout>
+                  <v-flex xs4>
+                    <v-btn text class="white--text" color="teal" to="/">
+                      <v-icon>mdi-account</v-icon>
+                    </v-btn>
+                  </v-flex>
+                  <v-flex xs4>
+                    <v-btn text class="white--text" color="teal" to="/skills">
+                      <v-icon>mdi-math-compass</v-icon>
+                    </v-btn>
+                  </v-flex>
+                  <v-flex xs4>
+                    <v-btn text class="white--text" color="teal" to="/activity">
+                      <v-icon>mdi-shoe-print</v-icon>
+                    </v-btn>
+                  </v-flex>
+                  <v-flex xs4>
+                    <v-btn text class="white--text" color="teal" to="/about">
+                      <v-icon>mdi-file-document</v-icon>
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-toolbar>
+          </v-flex>
           <v-flex id="main" md8 sm8 xs12 pa-12>
             <transition mode="out-in">
               <router-view />
